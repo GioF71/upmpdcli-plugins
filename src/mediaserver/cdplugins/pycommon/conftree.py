4cc3943f158b8d5f7cfdae443b99244cf5994819
Jean-Francois Dockes <jf@dockes.org>
2022-12-27 17:15:13 +0100

    bumped version

--- src/mediaserver/cdplugins/pycommon/conftree.py
+++ src/mediaserver/cdplugins/pycommon/conftree.py
@@ -14,6 +14,8 @@
 #   Free Software Foundation, Inc.,
 #   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+from __future__ import print_function
+
 import locale
 import re
 import os
