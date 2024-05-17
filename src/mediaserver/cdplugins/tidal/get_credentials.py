6b8f1471524cc3cded00cba3b13f848a50bb45a1
Giovanni Fulco <giovanni.fulco@gmail.com>
2024-05-17 08:49:44 +0200

    Tidal Plugin: get_credentials can optionally write to file
    - add a command line argument '-f' so that we can directly write the credentials file

--- src/mediaserver/cdplugins/tidal/get_credentials.py
+++ src/mediaserver/cdplugins/tidal/get_credentials.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright (C) 2023 Giovanni Fulco
+# Copyright (C) 2023,2024 Giovanni Fulco
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,6 +17,8 @@
 
 import tidalapi
 from datetime import datetime
+import argparse
+import json
 
 tidal_plugin_name : str = "tidal"
 
@@ -25,19 +27,42 @@ def print_setting(name : str, value : str):
     print(f"{tidal_plugin_name}{name} = {value}")
 
 
-session = tidalapi.Session()
-# Will run until you visit the printed url and link your account
-session.login_oauth_simple()
+def main():
+    parser = argparse.ArgumentParser(description='Tidal Credentials Retriever')
+    parser.add_argument(
+        '--f',
+        type=str,
+        help='Optionally write credentials to the specified file')
+    args = parser.parse_args()
+    if args.f:
+        print(f"Credentials will be written to [{args.f}]")
+    session = tidalapi.Session()
+    # Will run until you visit the printed url and link your account
+    session.login_oauth_simple()
+    token_type = session.token_type
+    access_token = session.access_token
+    refresh_token = session.refresh_token
+    expiry_time = session.expiry_time
+    storable_expiry_time = datetime.timestamp(expiry_time)
+    print("Settings for upmpdcli below, to be set in upmpdcli.conf")
+    print_setting("tokentype", token_type)
+    print_setting("accesstoken", access_token)
+    print_setting("refreshtoken", refresh_token)
+    print_setting("expirytime", storable_expiry_time)
+    if args.f:
+        print(f"Writing the credentials to file [{args.f}] ...")
+        # write a new json file
+        cred_dict : dict[str, str] = {
+            "authentication_type": "oauth2",
+            "tokentype": token_type,
+            "accesstoken": access_token,
+            "refreshtoken": refresh_token,
+            "expirytimetimestampstr": storable_expiry_time
+        }
+        with open(args.f, 'w') as cred_file:
+            json.dump(cred_dict, cred_file, indent = 4)
+        print(f"Credentials written to [{args.f}]")
 
-token_type = session.token_type
-access_token = session.access_token
-refresh_token = session.refresh_token
-expiry_time = session.expiry_time
 
-storable_expiry_time = datetime.timestamp(expiry_time)
-
-print("Settings for upmpdcli below, to be set in upmpdcli.conf")
-print_setting("tokentype", token_type)
-print_setting("accesstoken", access_token)
-print_setting("refreshtoken", refresh_token)
-print_setting("expirytime", storable_expiry_time)
+if __name__ == "__main__":
+    main()
