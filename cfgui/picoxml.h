2970233f3777d66aa75065f330bb8bc6cce762d6
Jean-Francois Dockes <jf@dockes.org>
2026-03-21 08:32:46 +0100

    shared

--- cfgui/picoxml.h
+++ cfgui/picoxml.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016 J.F.Dockes
+/* Copyright (C) 2016-2026 J.F.Dockes
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
